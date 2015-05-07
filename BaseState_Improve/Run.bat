dfebb5050b33c1011e586bf2747fb00f5825bbe9
yuanmouren1hao <342834599@qq.com>
2015-05-07 16:24:59 +0800

    初始化   基于过滤边的状态划分算法的工程

---
+++ BaseState_Improve/Run.bat
@@ -0,0 +1,14 @@
+@Rem This is a batch file for myflow algorithm
+start /wait Debug\myFlow.exe data\s-t\V6E10s-t.txt  data\mydata\V6E10.txt     data\results\V6E10.txt
+
+start /wait Debug\myFlow.exe data\s-t\V8E14s-t.txt  data\mydata\V8E14.txt     data\results\V8E14.txt
+
+start /wait Debug\myFlow.exe data\s-t\V10E18s-t.txt data\mydata\V10E18.txt    data\results\V10E18.txt
+
+start /wait Debug\myFlow.exe data\s-t\V12E22s-t.txt data\mydata\V12E22.txt    data\results\V12E22.txt
+
+start /wait Debug\myFlow.exe data\s-t\V14E26s-t.txt data\mydata\V14E26.txt    data\results\V14E26.txt
+
+start /wait Debug\myFlow.exe data\s-t\mys-t.txt data\mydata\TestGraph.txt    data\results\TestGraph.txt
+
+start /wait Debug\myFlow.exe data\s-t\new_graph.txt data\mydata\new_graph.txt  data\results\new_graph.txt
---
+++ BaseState_Improve/myFlow/InputReader.h
@@ -0,0 +1,29 @@
+#ifndef INPUTREADER_H
+#define INPUTREADER_H
+
+#include "ConstDef.h"
+#include <fstream>
+using namespace std;
+
+class Graph;
+
+class InputReader
+{
+public:
+	InputReader(const char* fileName, const char* stFileName);
+	~InputReader();
+
+	bool ReadGraph(Graph& g,int AllEdge[][5],double AllEdge_p[]);
+	void ReadSourceSink(int &s, int &t);
+
+private:
+	void ReadFirstLine();  /*读取（g # 1）*/
+
+private:
+	ifstream in;          /*读graph*/
+	ifstream stIn;        /*源点s汇点t*/
+	char strLine[32];     /*记录in读取的内容*/
+	int gId;
+};
+
+#endif
