c22cbc2379ecbdaf85d07fa8a2e482d01fb2475e
yuanmouren1hao <342834599@qq.com>
2015-05-07 16:05:57 +0800

    修改测试用例

--- BaseState/Run - 副本.bat
+++ BaseState/Run - 副本.bat
@@ -1,4 +1,2 @@
 @Rem This is a batch file for myflow algorithm
-start /wait E:\myFlow\Debug\myFlow.exe data\s-t\mys-t.txt data\mydata\TestGraph.txt    data\results\TestGraph.txt
-
-start /wait E:\myFlow\Debug\myFlow.exe data\s-t\new_graph.txt data\mydata\new_graph.txt  data\results\new_graph.txt
+start /wait Debug\myFlow.exe data\s-t\mys-t.txt data\mydata\TestGraph.txt    data\results\TestGraph.txt
