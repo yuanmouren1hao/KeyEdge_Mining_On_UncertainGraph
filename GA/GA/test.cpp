6807b24886b91cc9a89685b230f49c17a737ccba
yuanmouren1hao <342834599@qq.com>
2015-10-26 21:23:11 +0800

    添加测试文件

---
+++ GA/GA/test.cpp
@@ -0,0 +1,17 @@
+
+#include <STDIO.H>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main(){
+
+	cout<<"hello world"<<endl;
+	getchar();
+	return 0;
+}
