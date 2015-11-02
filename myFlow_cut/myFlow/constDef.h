26db9619db1ec8be8029961d83ff1d904d3eab48
yuanmouren1hao <342834599@qq.com>
2015-11-02 16:06:23 +0800

    改进的算法，割集过滤算法

---
+++ myFlow_cut/myFlow/constDef.h
@@ -0,0 +1,11 @@
+#ifndef CONSTDEF_H
+#define CONSTDEF_H
+
+#define MAX  (30)                                                /*图最大顶点数*/
+#define BUFFER_SIZE (256)                                        /*文件名长度*/ 
+#define MAX_E_NUM (100)                                          /*图最大边数*/
+#define MB (1048576)                                             /*1024*1024*/
+#define WORK_SPACE  ("E:\\myFlow_cut\\")							 /*当前项目主工作目录*/
+#define NUM_OF_SUB_GRAPH (1000)									 /*最大的下界子图的个数*/
+
+#endif
