39358364d68b703a39c31232b673a5b53d51adf7
yuanmouren1hao <342834599@qq.com>
2015-11-02 17:33:46 +0800

    修改自动化执行文件

--- myFlow_base/Run_test.bat
+++ myFlow_base/Run_test.bat
@@ -1,3 +1,5 @@
 @Rem This is a batch file for myflow algorithm
 
-start /wait E:\myFlow\Debug\myFlow.exe data\s-t\test_graph_st.txt data\mydata\TestGraph.txt    results\new\TestGraph.txt
+start /wait E:\myFlow_base\Debug\myFlow_base.exe data\s-t\test_graph_st.txt data\mydata\TestGraph.txt    results\new\TestGraph.txt
+
+start /wait E:\myFlow_base\Debug\myFlow_base.exe data\s-t\V6E10s-t.txt  data\mydata\V6E10.txt     results\new\V6E10.txt
