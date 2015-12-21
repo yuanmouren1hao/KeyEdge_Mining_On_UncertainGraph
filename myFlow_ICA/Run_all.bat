6c6ef9086555e73a913628408f0094be9fc61856
yuanmouren1hao <342834599@qq.com>
2015-12-21 21:39:10 +0800

    修改文件夹名称，先删除后添加

---
+++ myFlow_ICA/Run_all.bat
@@ -0,0 +1,12 @@
+@Rem This is a batch file for myflow algorithm
+start /wait E:\myFlow_ICA\Debug\myFlow_ICA.exe data\s-t\V6E10s-t.txt  data\mydata\V6E10.txt     results\new\V6E10.txt
+
+start /wait E:\myFlow_ICA\Debug\myFlow_ICA.exe data\s-t\V8E14s-t.txt  data\mydata\V8E14.txt     results\new\V8E14.txt
+
+start /wait E:\myFlow_ICA\Debug\myFlow_ICA.exe data\s-t\V10E18s-t.txt data\mydata\V10E18.txt    results\new\V10E18.txt
+
+start /wait E:\myFlow_ICA\Debug\myFlow_ICA.exe data\s-t\V12E22s-t.txt data\mydata\V12E22.txt    results\new\V12E22.txt
+
+start /wait E:\myFlow_ICA\Debug\myFlow_ICA.exe data\s-t\V14E26s-t.txt data\mydata\V14E26.txt    results\new\V14E26.txt
+
+start /wait E:\myFlow_ICA\Debug\myFlow_ICA.exe data\s-t\test_graph_st.txt data\mydata\TestGraph.txt    results\new\TestGraph.txt
