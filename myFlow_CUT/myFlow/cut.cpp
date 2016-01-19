fc8bc59645c98fe98e4318ad7a4cd79bc83d87b8
yuanmouren1hao <342834599@qq.com>
2016-01-19 22:27:34 +0800

    修改获取割集中的边和悬挂边的方法。

--- myFlow_CUT/myFlow/cut.cpp
+++ myFlow_CUT/myFlow/cut.cpp
@@ -3,13 +3,14 @@
 
 /*关于图的一些算法*/
 #include "graph.h"
+#include "cut.h"
 using namespace std;
 
 
 
 /*获取确定图中某个最大流对应割集中的边*/
-/*参数说明：g:不确定图；source:顶点；sink:终点； MinCutEdges：割边集合的引用 */
-int getMinCutEdges(Graph& g, int source, int sink, set<int>& MinCutEdges)
+/*参数说明：g:不确定图；source:顶点；sink:终点； certainEdge：确定边集合的引用 */
+int getMinCutEdges(Graph& g, int source, int sink,  CertainEdge& certainEdge)
 {
 	/*求得剩余图,其中gf为剩余图*/
 	Flow f;
@@ -17,7 +18,7 @@ int getMinCutEdges(Graph& g, int source, int sink, set<int>& MinCutEdges)
 	int max_flow = Dinic(g,source,sink,f,gf);
 
 	/*源测割点集*/
-	set<int>S;
+	set<int> S;
 	/*使用剩余图获取源测割点集合*/
 	MinCut(gf,g.nV, source, S);
 	/*起点在S中，终点不在S中的边为关键边*/
@@ -25,11 +26,63 @@ int getMinCutEdges(Graph& g, int source, int sink, set<int>& MinCutEdges)
 	{
 		if (S.count(g.AllEdge[i][1]) && !S.count(g.AllEdge[i][2]))
 		{
-			MinCutEdges.insert(g.AllEdge[i][4]);
+			certainEdge.MinCutEdges.insert(g.AllEdge[i][4]);
 		}
 	}
 
 	return 0;
 }
 
+/*获取确定的边，悬挂边*/
+/*参数说明：g:不确定图；source:顶点；sink:终点； certainEdge：确定边集合的引用 */
+void getHangEdge(Graph& g, int source, int sink, CertainEdge& certainEdge)
+{
+	set<int> in_set,out_set;/*起点in,终点out*/
+	for(int i=1; i<=g.nE; i++)
+	{
+		in_set.insert(g.AllEdge[i][1]);
+		out_set.insert(g.AllEdge[i][2]);
+	}
 
+	for(int i=1; i<=g.nE; i++)
+	{
+		/*源点流入的边是悬挂边*/
+		if (g.AllEdge[i][2] == source)
+		{
+			certainEdge.HangEdges.insert(g.AllEdge[i][4]);
+		}
+		/*汇点流出的边是悬挂边*/
+		if (g.AllEdge[i][1] == sink)
+		{
+			certainEdge.HangEdges.insert(g.AllEdge[i][4]);
+		}
+
+		/*只有流入，没有流出的点*/
+		if (in_set.count(g.AllEdge[i][2])==0 && out_set.count(g.AllEdge[i][2])>0 && sink != g.AllEdge[i][2])
+		{
+			certainEdge.HangEdges.insert(g.AllEdge[i][4]);
+		}
+
+		/*只有流出，没有流入的点*/
+		if (in_set.count(g.AllEdge[i][1])>0 && out_set.count(g.AllEdge[i][1])==0 && source!=g.AllEdge[i][1])
+		{
+			certainEdge.HangEdges.insert(g.AllEdge[i][4]);
+		}
+
+	}
+	//certainEdge.HangEdges.insert(0);
+	return;
+}
+
+
+/*获取确定的边，包括悬挂边和割集中的边*/
+/*参数说明：g:不确定图；source:顶点；sink:终点； certainEdge：确定边集合的引用 */
+void getCertainEdges(Graph& g, int source, int sink, CertainEdge& certainEdge)
+{
+	/*获取割集中的边*/
+	getMinCutEdges(g,source,sink,certainEdge);
+	/*获取悬挂边*/
+	getHangEdge(g,source,sink,certainEdge);
+
+	return;
+}
--- myFlow_CUT/myFlow/cut.h
+++ myFlow_CUT/myFlow/cut.h
@@ -9,10 +9,15 @@ typedef struct{
 }SplitGraph; 
 
 
+/*确定边的集合，包括割集中的边和悬挂边*/
+typedef struct{
+	set<int> MinCutEdges;/*割集中的边*/
+	set<int> HangEdges;/*悬挂边*/
+}CertainEdge;
 
 
-/*获取确定图中某个最大流对应割集中的边*/
-/*参数说明：g:不确定图；source:顶点；sink:终点； MinCutEdges：割边集合的引用 */
-int getMinCutEdges(Graph& g, int source, int sink, set<int>& MinCutEdges);
+/*获取确定的边，包括悬挂边和割集中的边*/
+/*参数说明：g:不确定图；source:顶点；sink:终点； certainEdge：确定边集合的引用 */
+void getCertainEdges(Graph& g, int source, int sink, CertainEdge& certainEdge);
 
 #endif
