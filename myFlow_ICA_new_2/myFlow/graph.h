9f879900df3cb4d9a2e4cd55ad6d4cc555a8324b
yuanmouren1hao <342834599@qq.com>
2016-03-02 20:54:59 +0800

    add ica_new_2

---
+++ myFlow_ICA_new_2/myFlow/graph.h
@@ -0,0 +1,79 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "ConstDef.h"
+#include <vector>
+using namespace std;
+
+struct Edge
+{
+	int iC;                                  /*边上容量*/
+	double dP;                               /*边可靠度*/
+	int iLabel;                              /*边的编号*/
+
+	Edge():iC(0),dP(0),iLabel(0){}
+};
+
+struct CP
+{
+	int iC; 
+	double dP;
+};                                           /*存放容量与概率*/
+
+typedef Edge AdjMatrix[MAX][MAX];            /*零行零列不用*/
+
+class Graph
+{
+public:
+	Graph();
+	~Graph();
+
+	void Init();                             /*图清零*/
+
+
+public: 
+	int nId;                                 /*图编号*/
+	int nV;                                  /*顶点数*/
+	int nE;                                  /*边数*/
+
+public:
+	AdjMatrix matrix;                        /*邻接矩阵*/
+	/*存储边的信息,不使用0*/
+	int AllEdge[MAX_E_NUM][5];/*1234，起点，终点，容量，有效位*/
+	double AllEdge_p[MAX_E_NUM];/**/
+
+	int max_flow;//最大流
+	double max_p1;//流分布可靠性
+	double max_p2;//网络可靠性
+};
+
+
+typedef int Flow[MAX][MAX];                  /*存放流的矩阵类型(0行0列不用)*/
+typedef int GF[MAX][MAX];                    /*剩余图类型(0行0列不用)*/
+
+struct MSN_STRUCT                            /*分成网络类型*/
+{
+	int nMSN[MAX][MAX];                      /*存放分层网络边的容量(0行0列不用)*/
+	int nVStage[MAX];                        /*记录分层网络中顶点的层次(0行0列不用)*/
+	int nV;                                  /*顶点个数(与原网络顶点个数相同)*/
+
+	void Init(int nVnum);
+}; 
+
+typedef struct MSN_STRUCT MSN;      
+
+struct BFnode
+{
+	int u;
+	int v;
+	int flow;
+};
+typedef struct BFnode BFLOW;                 /*阻塞流*/            
+
+/*Dinic算法*/
+int Dinic(Graph& g,int source,int sink,Flow& f,GF& gf);
+
+/*通过剩余图得到相应最大流的最小割*/
+void MinCut(GF& gf,int n,int source,vector<int>& S);
+
+#endif
---
+++ myFlow_ICA_new_2/myFlow/state.h
@@ -0,0 +1,79 @@
+#ifndef STATE_H
+#define STATE_H
+
+/*存储所有下界子图的状态*/
+typedef struct {
+	int State_Num;							/*下界子图的个数*/
+	int Edge_Num;							/*边的个数*/
+	int State[NUM_OF_SUB_GRAPH][MAX_E_NUM]; /*存储所有下界子图，一行表示一个子图，0不使用*/
+	//int State_upper[NUM_OF_SUB_GRAPH][MAX_E_NUM];/*存储所有子图上界，一行表示一个子图，0不使用*/
+}Lower_subGraph;
+
+
+/*定义关键边数据结构*/
+typedef struct{
+	int Edge;								/*边的编号*/
+	char Edge_Class;						/*边的类别，ABC类*/
+	double ChangeAmount_c;					/*流量的变化量*/
+	double ChangeAmount_p1;					/*随机流网络可靠性变化量*/
+	double ChangeAmount_p2;					/*流分布可靠性变化量*/
+}KeyEdge;
+/*定义关键边集合*/
+struct KeyEdgeSet{
+	int EdgeNum;							/*边的个数*/
+	int A_num;								/*A类边的个数*/
+	int B1_num;								/*B1类边的个数*/
+	int B2_num;								/*B2类边的个数*/
+	int C_num;								/*C类边的个数*/
+	KeyEdge A_EdgeInfo[MAX_E_NUM];			/*存储A边的信息，有排序,0不使用*/
+	KeyEdge B1_EdgeInfo[MAX_E_NUM];			/*存储B边的信息，有排序,0不使用*/
+	KeyEdge B2_EdgeInfo[MAX_E_NUM];			/*存储B边的信息，有排序,0不使用*/
+	KeyEdge C_EdgeInfo[MAX_E_NUM];			/*存储C边的信息，有排序,0不使用*/
+
+	/*默认构造函数*/
+	KeyEdgeSet()
+	{
+		EdgeNum = 0;
+		A_num = 0;
+		B1_num = 0;
+		B2_num = 0;
+		C_num = 0;
+
+		for (int i=0; i<MAX_E_NUM; i++)
+		{
+			A_EdgeInfo[i].ChangeAmount_c = 0;
+			A_EdgeInfo[i].ChangeAmount_p1 = 0;
+			A_EdgeInfo[i].ChangeAmount_p2 = 0;
+			A_EdgeInfo[i].Edge = 0;
+
+			B1_EdgeInfo[i].ChangeAmount_c = 0;
+			B1_EdgeInfo[i].ChangeAmount_p1 = 0;
+			B1_EdgeInfo[i].ChangeAmount_p2 = 0;
+			B1_EdgeInfo[i].Edge = 0;
+
+			B2_EdgeInfo[i].ChangeAmount_c = 0;
+			B2_EdgeInfo[i].ChangeAmount_p1 = 0;
+			B2_EdgeInfo[i].ChangeAmount_p2 = 0;
+			B2_EdgeInfo[i].Edge = 0;
+
+			C_EdgeInfo[i].ChangeAmount_c = 0;
+			C_EdgeInfo[i].ChangeAmount_p1 = 0;
+			C_EdgeInfo[i].ChangeAmount_p2 = 0;
+			C_EdgeInfo[i].Edge = 0;
+		}
+	};
+};
+
+
+
+//ica核心算法
+void computeICA(Lower_subGraph *StateMtrix, KeyEdgeSet *key_edge_set, Graph& g, int source, int sink);
+
+
+/*将关键边输出*/
+void printKeyEdge(ostream &file_out,KeyEdgeSet &key_edge_set);
+/*初始化关键边集，用于循环计算*/
+void init_KeyEdgeSet(KeyEdgeSet &key_edge_set,Lower_subGraph &StateMtrix);
+
+
+#endif
